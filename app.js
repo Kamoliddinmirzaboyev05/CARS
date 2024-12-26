e86b9065730e5227e914cec20e917ff4db045fdc
KamoliddinMirzaboyev <kamoliddinmirzaboyev05@gmail.com>
2024-12-26 16:37:12 +0500

    Add delete

--- app.js
+++ app.js
@@ -7,48 +7,61 @@ const submit = document.querySelector(".submit");
 const rightSide = document.querySelector(".right-side");
 
 var db = [];
-submit.addEventListener("click", () => {
+
+const writeData = () => {
   rightSide.innerHTML = "";
-  if(carImg.value != "" && carName.value != "" && carSpeed.value != "" && carPrice.value != "" ){
-   var addCar = {
-    img: carImg.value,
+  db.forEach((item) => {
+    rightSide.innerHTML += `
+        <div class="card">
+        <div class="car-img">
+            <img src=${item.img} alt="">
+        </div>
+        <div class="car-name">
+            <h2>${item.name}</h2>
+            <i onclick="removeCar(${item.id})" class="fas fa-trash"></i>
+        </div>
+        <div class="car-data data-border">
+            <p>Speed:</p>
+            <p>${item.speed} km/h</p>
+        </div>
+        <div class="car-data data-border">
+            <p>Price:</p>
+            <p>${item.price} $</p>
+        </div>
+        <div class="car-data data-border">
+            <p>Color:</p>
+            <p>${item.color}</p>
+        </div>
+        <div class="car-data">
+            <a href="#">More images</a>
+            <a>Another links</a>
+        </div>
+    </div>
+        `;
+  });
+};
+submit.addEventListener("click", () => {
+  var obj = {
+    id: Math.random() * 999,
     name: carName.value,
     speed: carSpeed.value,
     price: carPrice.value,
     color: carColor.value,
+    img: carImg.value,
   };
-  }else{
-    alert("Ma`lumotlar to'liq kiritilmadi!")
-  }
- 
-  db.push(addCar);
-  db.forEach((item) => {
-    rightSide.innerHTML += `
-          <div class="card">
-          <div class="car-img">
-              <img src=${item.img} alt="">
-          </div>
-          <div class="car-name">
-              <h2>${item.name}</h2>
-              <i class="fas fa-trash"></i>
-          </div>
-          <div class="car-data data-border">
-              <p>Speed:</p>
-              <p>${item.speed} km/h</p>
-          </div>
-          <div class="car-data data-border">
-              <p>Price:</p>
-              <p>${item.price} $</p>
-          </div>
-          <div class="car-data data-border">
-              <p>Color:</p>
-              <p>${item.color}</p>
-          </div>
-          <div class="car-data">
-              <a href="#">More images</a>
-              <a>Another links</a>
-          </div>
-      </div>
-          `;
-  });
+  db.push(obj);
+  writeData();
+  carName.value = "";
+  carSpeed.value = "";
+  carPrice.value = "";
+  carColor.value = "";
+  carImg.value = "";
 });
+
+var removeCar = (id) => {
+  var newData = db.filter((item) => {
+    return id != item.id;
+  });
+  db = newData;
+  writeData();
+};
